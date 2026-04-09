0e043cd1bce7318589b287f31fa5b441d6c7cea5
RocherSilencieux <164058459+RocherSilencieux@users.noreply.github.com>
2026-04-09 14:05:26 +0200

    fix: resolution des bugs d'accessibilite sonar et exclusion des dossiers generes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,7 +44,8 @@
 
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                                this.closest('form').submit();"
+                                    onkeydown="if(event.key === 'Enter') { event.preventDefault(); this.closest('form').submit(); }">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
@@ -90,7 +91,8 @@
 
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                                        this.closest('form').submit();"
+                            onkeydown="if(event.key === 'Enter') { event.preventDefault(); this.closest('form').submit(); }">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
